0c68546d5094954f8b0347837a9d12506fa64d25
Liutia <you@example.com>
2021-06-16 11:40:30 +0300

    Task-5

--- web/modules/custom/liutia/src/Form/ExForm.php
+++ web/modules/custom/liutia/src/Form/ExForm.php
@@ -20,6 +20,7 @@ class ExForm extends FormBase {
    '#title' => $this->t('Your cat’s name:'),
    '#description' => $this->t('Name: 2-32 letters'),
    '#required' => TRUE,
+   '#maxlength' => 32,
   ];
 
   // Add a submit button that handles the submission of the form.
@@ -34,6 +35,15 @@ class ExForm extends FormBase {
  // метод, который будет возвращать название формы
  public function getFormId() {
   return 'liutia_exform_form';
+ }
+
+ // ф-я валидации
+ public function validateForm(array &$form, FormStateInterface $form_state) {
+  $title = $form_state->getValue('title');
+  $is_number = preg_match("/\w{2,32}/", $title, $match);
+  if ($is_number <= 0) {
+   $form_state->setErrorByName('title', $this->t('Error'));
+  }
  }
 
  // действия по сабмиту
