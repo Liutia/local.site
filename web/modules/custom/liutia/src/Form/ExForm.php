fa452eb2d1310013037df0ec36f8b1d340f85088
Liutia <you@example.com>
2021-06-16 11:54:25 +0300

    Task-6

--- web/modules/custom/liutia/src/Form/ExForm.php
+++ web/modules/custom/liutia/src/Form/ExForm.php
@@ -2,53 +2,72 @@
 
 namespace Drupal\liutia\Form;
 
+use Drupal\Core\Ajax\HtmlCommand;
+use Drupal\Core\Ajax\InsertCommand;
 use Drupal\Core\Form\FormBase;                   // Базовый класс Form API
 use Drupal\Core\Form\FormStateInterface;              // Класс отвечает за обработку данных
+use Drupal\Core\Ajax\AjaxResponse;
+
+
 
 /**
- * Наследуемся от базового класса Form API
  * @see \Drupal\Core\Form\FormBase
  */
+
 class ExForm extends FormBase {
 
  public function buildForm(array $form, FormStateInterface $form_state) {
+   $form['text']['#markup'] = 'Hello! You can add here a photo of your cat.';
+   $form['message'] = [
+     '#type' => 'markup',
+     '#markup' => '<div class="result_message"></div>',
+   ];
 
-  $form['text']['#markup'] = 'Hello! You can add here a photo of your cat.';
+   $form['title'] = [
+     '#type' => 'textfield',
+     '#title' => $this->t('Your cat’s name:'),
+     '#description' => $this->t('Name: 2-32 letters'),
+     '#required' => TRUE,
+     '#maxlength' => 32,
+     ];
 
-  $form['title'] = [
-   '#type' => 'textfield',
-   '#title' => $this->t('Your cat’s name:'),
-   '#description' => $this->t('Name: 2-32 letters'),
-   '#required' => TRUE,
-   '#maxlength' => 32,
-  ];
+   $form['action'] = [
+     '#type' => 'submit',
+     '#value' => $this->t('Add cat'),
+     '#ajax' => [
+       'callback' => '::setMessage',
+     ]
+   ];
 
-  // Add a submit button that handles the submission of the form.
-  $form['actions']['submit'] = [
-   '#type' => 'submit',
-   '#value' => $this->t('Add cat'),
-  ];
-
-  return $form;
+   return $form;
  }
 
- // метод, который будет возвращать название формы
+ public function setMessage(array $form, FormStateInterface $form_state) {
+   $response = new AjaxResponse();
+   $title = $form_state->getValue('title');
+   $is_number = preg_match("/\w{2,32}/", $title, );
+   if ($is_number <= 0) {
+     $response->addCommand(
+       new HtmlCommand(
+         '.result_message',
+         '<div class="my_top_message">' . $this->t('You cat name no correct')
+       )
+     );
+   }
+   if ($is_number > 0) {
+     $response->addCommand(
+       new HtmlCommand(
+         '.result_message',
+         '<div class="my_top_message">' . $this->t('You cat name: %title.', ['%title' => $title])
+       )
+     );
+   }
+   return $response;
+ }
+
  public function getFormId() {
-  return 'liutia_exform_form';
+   return 'liutia_exform_form';
  }
 
- // ф-я валидации
- public function validateForm(array &$form, FormStateInterface $form_state) {
-  $title = $form_state->getValue('title');
-  $is_number = preg_match("/\w{2,32}/", $title, $match);
-  if ($is_number <= 0) {
-   $form_state->setErrorByName('title', $this->t('Error'));
-  }
- }
-
- // действия по сабмиту
- public function submitForm(array &$form, FormStateInterface $form_state) {
-  $title = $form_state->getValue('title');
-  \Drupal::messenger()->addMessage(t('You cat name: %title.', ['%title' => $title]));
- }
+ public function submitForm(array &$form, FormStateInterface $form_state) {}
 }
